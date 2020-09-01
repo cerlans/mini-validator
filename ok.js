4263c1ccef476e47b6bd9f2745cb209702a7306c
Kevin <35633054+cerlans@users.noreply.github.com>
2020-09-01 13:50:09 -0400

    Update ok.js
    Come back to finish up the password meter

--- ok.js
+++ ok.js
@@ -1,19 +1,16 @@
-var firstHeader = document.createElement('h1');
-firstHeader.innerText = 'Sign-Up Validator';
-firstHeader.style.textAlign = 'center';
+// unsure as to whether i have alot of redundancy or not
+// would like to reduce all these if staments, if i could
+function formClear() {
+    for (item of allInputs) {
+        item.value = '';
+    }
+}
 
-var script = document.body.getElementsByTagName('script');
-var firstClass = document.getElementsByClassName('firstDiv');
-document.body.appendChild(firstHeader);
-document.body.insertBefore(firstHeader, script[0]);
-document.body.insertBefore(firstClass[0], script[0]);
-
-
-var a = document.getElementsByTagName('input')[0]
-var y = document.getElementsByTagName('form')[0];
-var allInputs = document.getElementsByTagName('input');
-var placeholderP = document.createElement('p');
-y.insertBefore(placeholderP, y[2]);
+// dom retrieval
+let a = document.getElementsByTagName('input')[1]
+let y = document.getElementsByTagName('form')[0];
+let allInputs = document.getElementsByTagName('input');
+let innerDiv = document.getElementsByTagName('div');
 
 
 function userNameGuideBox() {
@@ -44,48 +41,76 @@ function userNameGuideBoxField() {
 
    }
 }
-
 function userNameGuideBoxOut() {
     innerDiv[2].style.display = 'none';
     let userNameGuide = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/gm
     if(userNameGuide.test(allInputs[0].value)) {
-        allInputs[0].style.border = 'none';
+        allInputs[0].style.border= '.0625rem solid #d4dbe0';
     } else {
         allInputs[0].style.border = 'solid red';
     }    
 }
 
-function formClear() {
-    for (item of allInputs) {
-        item.value = '';
-    }
-}
-
 function emailValidator() {
+    var emailP = document.getElementsByTagName('p');
     let emailString = /^.+[\@].+[\.](com|edu|net|org|gov|mil)$/gm;
-    if (emailString.test(a.value)) {
-        placeholderP.innerText = '';
-    } else {
-        placeholderP.innerText = 'Must be a Valid Email Format!'
+    if(emailString.test(a.value)) {
+        allInputs[1].style.border = '.0625rem solid #d4dbe0';
+        emailP[3].innerText = '';
     }
 };
 
+function emailValidatorOut() {
+    var emailP = document.getElementsByTagName('p');
 
-
-let g = document.getElementsByTagName('input')[4];
-let placeholdertwo = document.createElement('p');
-placeholdertwo.innerText = 'Number must be seperated by -';
-y.insertBefore(placeholdertwo, y[5]);
-
-function phoneValidator() {
-
-    let regEx = /^(203|408|860|959)[\-]\d{3}[\-]\d{4}$/gm
-    if (regEx.test(g.value)) {
-        placeholdertwo.innerText = 'This is a valid number!'
+    let emailString = /^.+[\@].+[\.](com|edu|net|org|gov|mil)$/gm;
+    if(emailString.test(a.value) !== true) {
+        allInputs[1].style.border = 'solid red';
+        emailP[3].innerText = 'Email must be in a valid email format (e.g., username@coolexample.com). Please Try Again.'
     } else {
-        placeholdertwo.innerText = 'Not a valid number!'
-    }
-    if (g.value === '') {
-        placeholdertwo.innerText = 'Number must be separated by -'
+        emailValidator();
     }
 }
+
+
+
+
+//input field for both phone validators, for the regex
+let g = document.getElementsByTagName('input')[3]
+// P tag for both phone validator functions
+let z =document.getElementsByTagName('p');
+function phoneValidator() {
+    let regEx = /^(203|408|860|959)[\-]\d{3}[\-]\d{4}$/gm
+
+    if(regEx.test(g.value)) {
+    z[5].innerText = ''
+    g.style.border = '.0625rem solid #d4dbe0';
+    };
+
+    if(g.value === '') {
+        z[5].innerText = 'Number must be seperated by -'
+    } 
+}
+function phoneValidatorOut() {
+    let regEx = /^(203|408|860|959)[\-]\d{3}[\-]\d{4}$/gm
+
+    if (regEx.test(g.value) !== true){
+    g.style.border = 'solid red';
+    z[5].innerText = 'Not a valid number!'
+    } else {
+    phoneValidator();
+}
+
+}
+function passwordGuideBox() {
+    innerDiv[3].style.display = 'block';
+}
+function passwordField() {
+
+}
+ function passwordGuideBoxOut() {
+    innerDiv[3].style.display = 'none';
+
+ }
+
+
